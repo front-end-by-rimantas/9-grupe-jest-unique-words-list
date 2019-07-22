219fa60f3b34e48976be5b2bcfed09f0603db6b8
Rimantas <belauzas@yahoo.com>
2019-07-22 10:23:04 +0300

    2D array and array[objects] unique words filtering

--- unique-words/unique-words.js
+++ unique-words/unique-words.js
@@ -1,5 +1,35 @@
-function uniqueWords() {
-    return;
+function uniqueWords( list2d, key='' ) {
+    let dictionary = [];
+
+    if ( !Array.isArray(list2d) ) {
+        return dictionary;
+    }
+
+    list2d.forEach( list => {
+        if ( Array.isArray(list) ) {
+            list.forEach( word => {
+                if ( typeof(word) === 'string' &&
+                     dictionary.indexOf(word) === -1 ) {
+                    dictionary.push( word );
+                }
+            });
+        } else {
+            if ( typeof(list) === 'object' &&
+                 list !== null &&
+                 typeof(key) === 'string' &&
+                 list[key] !== undefined &&
+                 Array.isArray( list[key] ) ) {
+                list[key].forEach( word => {
+                    if ( typeof(word) === 'string' &&
+                         dictionary.indexOf(word) === -1 ) {
+                        dictionary.push( word );
+                    }
+                });
+            }
+        }
+    });
+
+    return dictionary;
 }
 
 module.exports = uniqueWords;
--- unique-words/unique-words.test.js
+++ unique-words/unique-words.test.js
@@ -6,3 +6,69 @@ const uniqueWords = require('./unique-words.js');
 it('Correct test (1)', ()=>{
     expect( uniqueWords([['a', 'b'], ['a', 'c']]) ).toEqual(['a', 'b', 'c']);
 })
+
+it('Correct test (2)', ()=>{
+    expect( uniqueWords([['a', 'b'], ['c']]) ).toEqual(['a', 'b', 'c']);
+})
+
+it('Correct test (3)', ()=>{
+    expect( uniqueWords([]) ).toEqual([]);
+})
+
+it('Correct test (4)', ()=>{
+    expect( uniqueWords('asd') ).toEqual([]);
+})
+
+it('Correct test (5)', ()=>{
+    expect( uniqueWords(['a']) ).toEqual([]);
+})
+
+it('Correct test (6)', ()=>{
+    expect( uniqueWords([['a', 551, [], 'b'], ['a', true, 'c']]) ).toEqual(['a', 'b', 'c']);
+})
+
+it('Correct test (7)', ()=>{
+    expect( uniqueWords() ).toEqual([]);
+})
+
+
+const works = [
+    {
+        img: 'serlokas.png',
+        title: 'Sherlokas',
+        tags: ['html', 'css']
+    },
+    {
+        img: 'zalia-mergaite.png',
+        title: 'Zalia mergaite',
+        tags: ['html', 'css']
+    },
+    {
+        img: 'sign-up.png',
+        title: 'Sign up',
+        tags: ['html', 'css', 'advanced']
+    },
+    {
+        img: 'coming-soon.png',
+        title: 'Coming soon',
+        tags: ['html', 'css', 'responsive']
+    },
+    {
+        img: 'car-racing.png',
+        title: 'Racing game',
+        tags: ['css', 'js']
+    },
+    {
+        img: 'portfolio.png',
+        title: 'Portfolio',
+        tags: ['responsive', 'js', 'advanced']
+    }
+];
+
+it('Correct test (8)', ()=>{
+    expect( uniqueWords( works, 'tags' ) ).toEqual(['html', 'css', 'advanced', 'responsive', 'js']);
+})
+
+it('Correct test (9)', ()=>{
+    expect( uniqueWords( works, 'hack' ) ).toEqual([]);
+})
